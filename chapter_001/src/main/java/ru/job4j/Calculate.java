f5da106b723f2eba35599b90a6524d9171c02702
ZubovVP <Zubov.VP@yandex.ru>
2017-08-12 22:11:59 +0300

    Made some changes

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,6 @@
+package ru.job4j;
+public class Calculate{
+public static void main (String[] args){
+	System.out.print("Hello world.");
+}
+}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+**
+* Package for calculate task.
+*
+* @author Vitaly Zubov (mailto:Zubov.VP@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
